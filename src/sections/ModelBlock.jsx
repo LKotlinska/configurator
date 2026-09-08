b9e496daeb7b733501de879c11a916f2f2c1c754
Your Name <laura.kotlinska.96@gmail.com>
2026-09-08 10:24:08 +0200

    Add section area for displaying 3d model

---
+++ src/sections/ModelBlock.jsx
@@ -0,0 +1,8 @@
+import styles from './ModelBlock.module.css'
+export default function ModelBlock() {
+    return(
+        <section className={styles.modelBlock}>
+            <h2>3d model will be displayed here</h2>
+        </section>
+    )
+}
