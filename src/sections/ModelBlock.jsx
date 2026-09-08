b14a3a851cea806720e1c9dc0388734a1e090ec0
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-09-08 14:45:25 +0200

    Successfully rendering of GLB file

--- src/sections/ModelBlock.jsx
+++ src/sections/ModelBlock.jsx
@@ -1,8 +1,11 @@
-import styles from './ModelBlock.module.css'
+import styles from "./ModelBlock.module.css";
+import Preview from "../3D-components/preview";
+
 export default function ModelBlock() {
-    return(
-        <section className={styles.modelBlock}>
-            <h2>3d model will be displayed here</h2>
-        </section>
-    )
+  return (
+    <section className={styles.modelBlock}>
+      <h2>3d model will be displayed here</h2>
+      <Preview />
+    </section>
+  );
 }
