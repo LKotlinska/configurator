621bee4cd7d80eff512be29a2d8eeda45d923628
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-09-11 11:03:44 +0200

    Removed deprecated code lines

--- src/sections/ModelBlock.jsx
+++ src/sections/ModelBlock.jsx
@@ -1,15 +1,10 @@
 import styles from "./ModelBlock.module.css";
-import Preview from "../3D-components/preview";
 import Chair from "../3D-components/Object";
 
 export default function ModelBlock() {
   return (
     <section className={styles.modelBlock}>
-
-      <h2>3d model will be displayed here</h2>
-      {/* <Preview /> */}
       <Chair />
-
     </section>
   );
 }
