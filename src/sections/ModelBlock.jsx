c60cbbd3691e4a57b31a40ebc74374fec845f206
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-09-11 11:34:48 +0200

    Added and positioned icon for 3D rotation

--- src/sections/ModelBlock.jsx
+++ src/sections/ModelBlock.jsx
@@ -5,11 +5,10 @@ import Chair from "../3D-components/Object";
 export default function ModelBlock() {
   return (
     <section className={styles.modelBlock}>
-
       <h2>3d model will be displayed here</h2>
       {/* <Preview /> */}
       <Chair />
-
+      <img src="/rotationIcon.png" className={styles.rotationIcon} />
     </section>
   );
 }
