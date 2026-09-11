81610a98d6d0c92e1f076a0becf23f885488f304
Your Name <laura.kotlinska.96@gmail.com>
2026-09-11 14:18:04 +0200

    Remove unused useState import

--- src/3D-components/Object.jsx
+++ src/3D-components/Object.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
