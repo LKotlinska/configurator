00cafe76e18bc0fb130b0278fd59076640b0fdfd
Your Name <laura.kotlinska.96@gmail.com>
2026-09-15 13:46:43 +0200

    Add Material accordion item to ProductBlock component

--- src/sections/ProductBlock.jsx
+++ src/sections/ProductBlock.jsx
@@ -69,6 +69,9 @@ export default function ProductBlock({ variant, onVariantChange, armrestOption, 
                         }
                     />
                     <AccordionItem 
+                        title="Material"
+                    />
+                    <AccordionItem 
                         title="Colour"
                     />
 
